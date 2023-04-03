83870153f6aa57ca0e3ae89f07374edf621aa254
Imran Fazilov <ifazilov3584@bths.edu>
2023-04-03 12:31:23 -0400

    period end

--- src/SimpleGUI.java
+++ src/SimpleGUI.java
@@ -1,2 +1,181 @@
-package PACKAGE_NAME;public class SimpleGUI {
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.JSlider;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import java.awt.Font;
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+
+public class SimpleGUI extends JFrame implements ActionListener, ItemListener, ChangeListener {
+    private JCheckBox checkBox1;
+    private JCheckBox checkBox2;
+    private JLabel welcomeLabel;
+    private JTextField textField;
+    private JTextArea textArea;
+    public SimpleGUI() {
+        super("Frame title");
+        init();
+    }
+
+    private void init() {
+        // setting up the frame
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(500, 400);
+        setLocation(300, 50);
+
+        // create the MenuBar and menu components
+        JMenuBar menuBar = new JMenuBar();
+        JMenu menu1 = new JMenu("File");
+        JMenuItem menuItem1 = new JMenuItem("Open");
+        JMenuItem menuItem2 = new JMenuItem("Save as");
+        menu1.add(menuItem1);
+        menu1.add(menuItem2);
+        JMenu menu2 = new JMenu("Help");
+        JMenuItem menuItem3 = new JMenuItem("FAQ");
+        JMenuItem menuItem4 = new JMenuItem("About");
+        menu2.add(menuItem3);
+        menu2.add(menuItem4);
+
+        // add "File" and "Help" menus to the MenuBar
+        menuBar.add(menu1);
+        menuBar.add(menu2);
+
+        // create the big text area located in the middle
+        textArea = new JTextArea();
+        textField = new JTextField(10);
+
+        // create welcome label
+        JLabel welcomeLabel = new JLabel("Welcome to my GUI!");
+        welcomeLabel.setFont(new Font("Helvetica", Font.BOLD, 20));
+
+        // create slider and adjust settings
+        JSlider slider = new JSlider(0, 40, 20);
+        slider.setMinorTickSpacing(1);
+        slider.setMajorTickSpacing(5);
+        slider.setPaintTicks(true);
+        slider.setPaintLabels(true);
+
+        // create a panel for organizing the label and slider
+        JPanel sliderPanel = new JPanel();
+
+        // add label and slider, in left-to-right order
+        sliderPanel.add(welcomeLabel);
+        sliderPanel.add(slider);
+
+        // create the components at the bottom
+        JLabel label = new JLabel("Enter Text");
+        JTextField textField = new JTextField(10); // accepts upto 10 characters
+        JButton sendButton = new JButton("Send");
+        sendButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                welcomeLabel.setText("Send pressed!");
+                String textBoxText = textField.getText();
+                textArea.append(textBoxText);
+            }
+        });
+        JButton resetButton = new JButton("Reset");
+        resetButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                welcomeLabel.setText("Reset pressed!");
+                textField.setText("");
+                textArea.setText("");
+                checkBox1.setSelected(false);
+                checkBox2.setSelected(false);
+            }
+        });
+
+        // create checkboxes
+        JCheckBox checkBox1 = new JCheckBox("Yes");
+        checkBox1.setBounds(100, 100, 50, 50);
+        JCheckBox checkBox2 = new JCheckBox("No", true);
+        checkBox2.setBounds(100, 150, 50, 50);
+
+        // create a panel for organizing the components at the bottom
+        JPanel panel = new JPanel(); // a "panel" is not visible
+
+        // add bottom components to the panel, in left-to-right order
+        panel.add(label);
+        panel.add(textField);
+        panel.add(sendButton);
+        panel.add(resetButton);
+        panel.add(checkBox1);
+        panel.add(checkBox2);
+
+        // creating a third panel to place slider and bottom panels vertically
+        // (allows two rows of UI elements to be displayed)
+        JPanel combinedPanels = new JPanel();
+        combinedPanels.setLayout(new GridLayout(2, 1));
+        combinedPanels.add(sliderPanel, BorderLayout.NORTH);
+        combinedPanels.add(panel, BorderLayout.SOUTH);
+
+        // add the menu bar to the TOP of the frame, the big white text area
+        // to the MIDDLE of the frame, and the "combinedPanels" (which has
+        // the label, slider, text box, buttons, and checkboxes) at the BOTTOM
+        add(menuBar, BorderLayout.NORTH);
+        add(textArea, BorderLayout.CENTER);
+        add(combinedPanels, BorderLayout.SOUTH);
+
+        // display the frame!
+        setVisible(true);
+
+        menuItem1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                textField.setText("Open");
+            }
+        });
+        menuItem2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                textField.setText("Save As");
+            }
+        });
+        menuItem3.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                textField.setText("FAQ");
+            }
+        });
+        menuItem4.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                textField.setText("About");
+            }
+        });
+
+        slider.addChangeListener(this);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    @Override
+    public void itemStateChanged(ItemEvent e) {
+
+    }
+
+    @Override
+    public void stateChanged(ChangeEvent e) {
+        Object source = e.getSource();
+        JSlider slider = (JSlider) source;
+        int value = slider.getValue();
+        textArea.setText("" + value);
+    }
 }
